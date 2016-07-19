0cc3295cf549856fd8750154e7c836aff791ec6a
kyh <kyhfan@gmail.com>
2016-07-19 17:53:33 +0900

    기본 설정

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+	session_start();
+    header("Content-Type: text/html; charset=UTF-8");
+	//환경설정 파일
+	include_once "include/global.php"; 			//변수정보
+	include_once "include/function.php"; 		//함수정보
+	include_once "include/dbi.php"; 			//DB 연결정보
+	//include_once "include/page.class.php";		//페이징 처리 CLASS
+	//include_once "include/phprandom.php";		//난수번호 생성 CLASS
+
+	mysqli_query ($my_db,"set names utf8");
+
+	$mobile_agent = array("iPhone","iPod","iPad","Android","Blackberry","SymbianOS|SCH-M\d+","Opera Mini", "Windows ce", "Nokia", "sony" );
+	$check_mobile = "N";
+	for($i=0; $i<sizeof($mobile_agent); $i++){
+		if(stripos( $_SERVER['HTTP_USER_AGENT'], $mobile_agent[$i] )){
+			$check_mobile = "Y";
+			break;
+		}
+	}
+
+	if($check_mobile == "Y")
+		$gubun = "MOBILE";
+	else
+		$gubun = "PC";
+?>
