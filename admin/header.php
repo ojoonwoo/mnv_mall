5eb00c50894b62e3506c9c6013bf710a50346c05
kyh <kyhfan@gmail.com>
2016-07-20 10:54:30 +0900

    카테고리 기능 작업중

---
+++ admin/header.php
@@ -0,0 +1,11 @@
+<?
+	include_once "../config.php";
+?>
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <script type="text/javascript" src="../js/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="../js/main.js"></script>
+    <title>Document</title>
+  </head>
---
+++ header.php
@@ -0,0 +1,10 @@
+<?
+	include_once "config.php";
+?>
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <script type="text/javascript" src="../js/main.js"></script>
+    <title>Document</title>
+  </head>
