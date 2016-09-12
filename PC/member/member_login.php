95aca5335710944011130c5ccd60a1f81a0922a4
kyh <kyhfan@gmail.com>
2016-09-12 19:56:31 +0900

    112221

--- PC/member/member_login.php
+++ PC/member/member_login.php
@@ -9,8 +9,20 @@
         <div class="area_top">
           <div class="head_bar clearfix">
             <ul class="user_status">
-              <li><a href="#"><span>로그인</span></a></li>
-              <li><a href="#"><span>회원가입</span></a></li>
+<?
+	if ($_SESSION['ss_chon_id'])
+	{
+?>
+              <li><a href="#" id="mb_logout"><span>로그아웃</span></a></li>
+              <li><a href="<?=$_mnv_PC_member_url?>modify_form.php"><span>정보수정</span></a></li>
+<?
+	}else{
+?>
+              <li><a href="<?=$_mnv_PC_member_url?>member_login.php"><span>로그인</span></a></li>
+              <li><a href="<?=$_mnv_PC_member_url?>join_form.php"><span>회원가입</span></a></li>
+<?
+	}
+?>
               <li><a href="#"><span>마이페이지</span></a></li>
               <li><a href="#"><span>장바구니</span></a></li>
               <li><a href="#"><span>주문조회</span></a></li>
