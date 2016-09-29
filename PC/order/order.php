5130cc850577ecef28bfd09cd23c0f092984fb84
kyh <kyhfan@gmail.com>
2016-09-29 17:13:09 +0900

    주문서 수정

--- PC/order/order.php
+++ PC/order/order.php
@@ -164,7 +164,7 @@
                   </div>
                   <div class="block_col line3">
                     <input class="inputT" type="text" size="6" id="order_zipcode" value="<?=$member_info['mb_zipcode']?>">
-                    <input type="button" value="우편번호" class="inputB"><br>
+                    <input type="button" value="우편번호" class="inputB" id="zip1"><br>
                     <input class="inputT" type="text" size="50" id="order_address1" value="<?=$member_info['mb_address1']?>"><span class="address_comment">기본주소</span><br>
                     <input class="inputT" type="text" size="50" id="order_address2" value="<?=$member_info['mb_address2']?>"><span class="address_comment">나머지주소(직접입력)</span>
                   </div>
@@ -243,7 +243,7 @@
                     </div>
                     <div class="block_col line3">
                       <input class="inputT" type="text" size="6" id="deliver_zipcode">
-                      <input type="button" value="우편번호" class="inputB"><br>
+                      <input type="button" value="우편번호" class="inputB" id="zip2"><br>
                       <input class="inputT" type="text" size="50" id="deliver_address1"><span class="address_comment">기본주소</span><br>
                       <input class="inputT" type="text" size="50" id="deliver_address2"><span class="address_comment">나머지주소(직접입력)</span>
                     </div>
@@ -368,15 +368,85 @@
     </div>
   </body>
 <script>
-	function amount_change(type, num) {
-		var amount = $('#buy_cnt'+num);
-		if(type=='up'){
-			amount.val(parseInt(amount.val())+1);
-		}else if(type=='down' && amount.val()>1){
-			amount.val(parseInt(amount.val())-1);
-		}else{
-			amount.val(1);
-		}
-	}
+	$('#zip1').on('click', function() {
+		new daum.Postcode({
+			oncomplete: function(data) {
+				// 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+				// 각 주소의 노출 규칙에 따라 주소를 조합한다.
+				// 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+				var fullAddr = ''; // 최종 주소 변수
+				var extraAddr = ''; // 조합형 주소 변수
+
+				// 사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+				if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+					fullAddr = data.roadAddress;
+				} else { // 사용자가 지번 주소를 선택했을 경우(J)
+					fullAddr = data.jibunAddress;
+				}
+
+				// 사용자가 선택한 주소가 도로명 타입일때 조합한다.
+				if(data.userSelectedType === 'R'){
+					//법정동명이 있을 경우 추가한다.
+					if(data.bname !== ''){
+						extraAddr += data.bname;
+					}
+					// 건물명이 있을 경우 추가한다.
+					if(data.buildingName !== ''){
+						extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+					}
+					// 조합형주소의 유무에 따라 양쪽에 괄호를 추가하여 최종 주소를 만든다.
+					fullAddr += (extraAddr !== '' ? ' ('+ extraAddr +')' : '');
+				}
+
+				// 우편번호와 주소 정보를 해당 필드에 넣는다.
+				document.getElementById('order_zipcode').value = data.zonecode; //5자리 새우편번호 사용
+				document.getElementById('order_address1').value = fullAddr;
+
+				// 커서를 상세주소 필드로 이동한다.
+				document.getElementById('order_address2').focus();
+			}
+		}).open();
+	});
+
+	$('#zip2').on('click', function() {
+		new daum.Postcode({
+			oncomplete: function(data) {
+				// 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+				// 각 주소의 노출 규칙에 따라 주소를 조합한다.
+				// 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+				var fullAddr = ''; // 최종 주소 변수
+				var extraAddr = ''; // 조합형 주소 변수
+
+				// 사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+				if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+					fullAddr = data.roadAddress;
+				} else { // 사용자가 지번 주소를 선택했을 경우(J)
+					fullAddr = data.jibunAddress;
+				}
+
+				// 사용자가 선택한 주소가 도로명 타입일때 조합한다.
+				if(data.userSelectedType === 'R'){
+					//법정동명이 있을 경우 추가한다.
+					if(data.bname !== ''){
+						extraAddr += data.bname;
+					}
+					// 건물명이 있을 경우 추가한다.
+					if(data.buildingName !== ''){
+						extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+					}
+					// 조합형주소의 유무에 따라 양쪽에 괄호를 추가하여 최종 주소를 만든다.
+					fullAddr += (extraAddr !== '' ? ' ('+ extraAddr +')' : '');
+				}
+
+				// 우편번호와 주소 정보를 해당 필드에 넣는다.
+				document.getElementById('deliver_zipcode').value = data.zonecode; //5자리 새우편번호 사용
+				document.getElementById('deliver_address1').value = fullAddr;
+
+				// 커서를 상세주소 필드로 이동한다.
+				document.getElementById('deliver_address2').focus();
+			}
+		}).open();
+	});
+
 </script>
 </html>
