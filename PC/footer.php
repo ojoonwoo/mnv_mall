152a70c027c63454a11fe8ef2a05783aea44b836
kyh <kyhfan@gmail.com>
2016-10-28 17:03:43 +0900

    푸터 수정

--- PC/footer.php
+++ PC/footer.php
@@ -2,7 +2,7 @@
   <div class="area_infoChon">
     <div class="inner infoC clearfix">
       <div class="box_info">
-        <span class="customerC"><img src="./images/customer_center.png" alt="고객센터"></span>
+        <span class="customerC"><img src="<?=$_mnv_PC_images_url?>customer_center.png" alt="고객센터"></span>
         <span class="telNum">070-4888-3580</span>
         <span>운영시간 10:30-18:00 / 점심시간 13:00-2:30</span>
         <span>우리은행 1005-102-344909 예금주 미니버타이징(주)</span>
@@ -27,23 +27,23 @@
     </div>
   </div>
   <div class="address">
-    <p><span>상호명</span> 촌의감각  <span>사업자등록번호</span> 114-87-11622 <a href="http://www.ftc.go.kr/info/bizinfo/communicationView.jsp?apv_perm_no=2016321015330201907&area1=&area2=&currpage=1&searchKey=04&searchVal=1148711622&stdate=&enddate=">[사업자정보확인]</a> <span>통신판매업</span> 2016-서울서초-1907</p>
+    <p><span>상호명</span> 촌의감각  <span>사업자등록번호</span> 114-87-11622 <a href="http://www.ftc.go.kr/info/bizinfo/communicationView.jsp?apv_perm_no=2016321015330201907&area1=&area2=&currpage=1&searchKey=04&searchVal=1148711622&stdate=&enddate=" target="_blank">[사업자정보확인]</a> <span>통신판매업</span> 2016-서울서초-1907</p>
     <p><span>대표자</span> 양선혜  <span>주소</span> 서울시 서초구 방배동 931-9 2층  <span>개인정보관리책임자</span> 김영훈  <span>호스팅서비스</span> 카페24</p>
     <p><span>메일</span> chon_life@naver.com  <span>고객센터</span> 070-4888-3580  <span>팩스</span> 02-532-2494</p>
     <p><a href="<?=$_mnv_PC_url?>privacy.php">privacy policy</a> | <a href="<?=$_mnv_PC_url?>termsOfUse.php">terms of use</a>
     </p>
     <p>@chon all rights reserved</p>
     <div class="payBlock">
-      <a href="#">
+      <a href="http://pgweb.uplus.co.kr/pg/wmp/mertadmin/jsp/mertservice/s_escrowYn.jsp?mertid=miniver" target="_blank">
         <img src="<?=$_mnv_PC_images_url?>uplusImg.png" alt="유플러스 전자결제" style="vertical-align:super;">
       </a>
       <img src="<?=$_mnv_PC_images_url?>footerPayImgSet.png" alt="LG U+, escrow, 현금영수증 가맹점" style="margin-left:-12px;">
     </div>
     <div class="snsBlock">
-      <a href="http://blog.naver.com/chon_life">
+      <a href="http://blog.naver.com/chon_life" target="_blank">
         <img src="<?=$_mnv_PC_images_url?>footer_blog_img.png" alt="촌의감각 블로그">
       </a>
-      <a href="https://www.instagram.com/chon.life">
+      <a href="https://www.instagram.com/chon.life" target="_blank">
         <img src="<?=$_mnv_PC_images_url?>footer_insta_img.png" alt="촌의감각 인스타" style="vertical-align:top;">
       </a>
       <a href="#">
